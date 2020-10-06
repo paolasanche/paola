8f8cbae083882f44d437769ae3f59faa21300abc
paolasanche <al221811737@gmail.com>
2020-10-05 22:17:28 -0500

    crear vista en controlador

---
+++ database/migrations/2020_10_06_025913_create_alumnos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlumnosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alumnos', function (Blueprint $table) {
+            $table->bigIncrements('idalumnos');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('email');
+            $table->string('edad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alumnos');
+    }
+}
---
+++ resources/views/paola.blade.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>VISTAS CONTROLADOR</title>
+		
+	</head>
+	<body>
+mi primera vista desde mi controlador
+	</body>
+</html>
